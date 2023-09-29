d9c5abb11967e24de3cfacd3cf91f6e3c829db65
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-09-29 10:19:14 +0100

    Added a simple multiplier

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("welcome to the Calculator");
+        Console.WriteLine("Please enter your first number");
+        double? firstNum = Double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter your second number");
+        double? secondNum = Double.Parse(Console.ReadLine());
+        Console.WriteLine("Your answer is: " + (firstNum * secondNum));
+
     }
 }
