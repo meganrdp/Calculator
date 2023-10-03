62caf2fafa62200d84ccd8b50373c541b29d401e
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-03 10:43:55 +0100

    Added the use of different operators

--- Program.cs
+++ Program.cs
@@ -5,11 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("welcome to the Calculator");
+        Console.WriteLine("Please enter the operator +, -, * or /");
+        string? whichOperator = Console.ReadLine();
         Console.WriteLine("Please enter your first number");
         double? firstNum = Double.Parse(Console.ReadLine());
         Console.WriteLine("Please enter your second number");
         double? secondNum = Double.Parse(Console.ReadLine());
-        Console.WriteLine("Your answer is: " + (firstNum * secondNum));
+
+        if(whichOperator == "+"){
+            Console.WriteLine("Your answer is:" + (firstNum + secondNum));
+        }
+        else if(whichOperator == "-"){
+            Console.WriteLine("Your answer is:" + (firstNum - secondNum));
+        }
+        else if(whichOperator == "/"){
+            Console.WriteLine("Your answer is:" + (firstNum / secondNum));
+        }
+        else if(whichOperator == "*"){
+            Console.WriteLine("Your answer is:" + (firstNum * secondNum));
+        }
+        else{
+            Console.WriteLine("Sorry I don't know that operator!");
+        }
+        Console.ReadLine();
+
+        //Console.WriteLine("Your answer is: " + (firstNum * secondNum));
 
     }
 }
