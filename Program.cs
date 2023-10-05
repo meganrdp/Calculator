b6aefb3bb0ecb4a33e18f80d0f526b1b9567f594
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-05 09:49:25 +0100

    Added the ability to use multiple numbers

--- Program.cs
+++ Program.cs
@@ -4,15 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
+        
+//request an opperator to use for the calculation
         Console.WriteLine("welcome to the Calculator");
         Console.WriteLine("Please enter the operator +, -, * or /");
         string? whichOperator = Console.ReadLine();
-        Console.WriteLine("Please enter your first number");
-        double? firstNum = Double.Parse(Console.ReadLine());
-        Console.WriteLine("Please enter your second number");
-        double? secondNum = Double.Parse(Console.ReadLine());
+        Console.WriteLine("how many numbers do you want to use? ");
+        double howMany = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("What is your first number? ");
+        double firstNum = Convert.ToDouble(Console.ReadLine());
 
-        if(whichOperator == "+"){
+        double yourAnswer = firstNum;
+        
+        for (int i = 1; i < howMany; i++) {
+            Console.Write("Please enter a number: ");
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+
+            if(whichOperator == "+"){
+                yourAnswer = yourAnswer + userNumber;
+            }
+            else if(whichOperator == "-"){
+                yourAnswer = yourAnswer - userNumber;;
+            }
+            else if(whichOperator == "/"){
+                yourAnswer = yourAnswer / userNumber;;
+            }
+            else if(whichOperator == "*"){
+                yourAnswer = yourAnswer * userNumber;;
+            }
+               
+
+        }
+         Console.WriteLine("The answer is" + yourAnswer);
+        
+
+
+
+
+        /*if(whichOperator == "+"){
             Console.WriteLine("Your answer is:" + (firstNum + secondNum));
         }
         else if(whichOperator == "-"){
@@ -26,7 +55,9 @@ class Program
         }
         else{
             Console.WriteLine("Sorry I don't know that operator!");
-        }
+        }*/
+
+
         Console.ReadLine();
 
         //Console.WriteLine("Your answer is: " + (firstNum * secondNum));
