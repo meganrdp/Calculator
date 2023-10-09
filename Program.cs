107212b7dc8d6372d8cbf4b60db8c90476a60ee3
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-09 13:20:33 +0100

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,47 +1,54 @@
 ﻿namespace Calculator;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    static void Fizzbuzz()
     {
-        
-// request an opperator to use for the calculation
-        Console.WriteLine("welcome to the Calculator");
+        Console.WriteLine("Welcome to The Calculator!");
+    }
+
+    static void CompleteOneCalculation()
+    {
         Console.WriteLine("Please enter the operator +, -, * or /");
         string? whichOperator = Console.ReadLine();
         Console.WriteLine("how many numbers do you want to use? ");
         double howMany = Convert.ToDouble(Console.ReadLine());
-
-// asked for first number so that it can be stored and subsequent numbers added/taken et from that
         Console.WriteLine("What is your first number? ");
         double firstNum = Convert.ToDouble(Console.ReadLine());
 
-// storing the first number in the variable to be worked on
         double yourAnswer = firstNum;
 
-// using for loop to run if statements and stop when up to the users number       
-        for (int i = 1; i < howMany; i++) {
+        for (int i = 1; i < howMany; i++)
+        {
             Console.Write("Please enter a number: ");
             double userNumber = Convert.ToDouble(Console.ReadLine());
 
-// bit clunky... is there an easier way to do this?
-            if(whichOperator == "+"){
+            if (whichOperator == "+")
+            {
                 yourAnswer = yourAnswer + userNumber;
             }
-            else if(whichOperator == "-"){
-                yourAnswer = yourAnswer - userNumber;;
+            else if (whichOperator == "-")
+            {
+                yourAnswer = yourAnswer - userNumber;
             }
-            else if(whichOperator == "/"){
-                yourAnswer = yourAnswer / userNumber;;
+            else if (whichOperator == "/")
+            {
+                yourAnswer = yourAnswer / userNumber;
             }
-            else if(whichOperator == "*"){
-                yourAnswer = yourAnswer * userNumber;;
+            else if (whichOperator == "*")
+            {
+                yourAnswer = yourAnswer * userNumber;
             }
-
         }
-        Console.WriteLine("The answer is" + yourAnswer);
-           
+
+        Console.WriteLine("The answer is " + yourAnswer);
         Console.ReadLine();
-       
+
+    }
+    
+    static void Main(string[] args)
+    {
+        Fizzbuzz();
+        CompleteOneCalculation();
     }
 }
